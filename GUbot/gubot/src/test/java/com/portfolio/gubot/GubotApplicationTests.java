1df6afefbcc735c4e603c3c352117b35c5a6c50c
00Hwang <rahyun3746@gmail.com>
2024-11-09 00:16:53 +0900

    스프링부트 초기세팅
    스프링부트 개발환경 초기세팅

---
+++ GUbot/gubot/src/test/java/com/portfolio/gubot/GubotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.gubot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GubotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
