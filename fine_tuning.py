652eb804448c28546af313b6d9fade1878e34d1e
Flier <rmflsdl4@gmail.com>
2024-12-18 18:53:45 +0900

    LLM 테스트, 파인튜닝 코드 갱신
    1. 파인튜닝 프롬프트를 수정해서 학습 진행했으나 결과가 만족스럽진 않음
    2. 모델 테스트에서 이전에 질문-답변 형식이 한 번만 나오는게 아니라 반복적으로 나오는 현상 수정

--- fine_tuning.py
+++ fine_tuning.py
@@ -1,78 +1,80 @@
 # CSV Setting
 import pandas as pd
 from datasets import Dataset
+from huggingface_hub import login
+#from langchain import hub
+#login(token="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")
 
 # CSV file read
-csv_path = "gu_datasets.csv"
+csv_path = "gu_datasets_no_context.csv"  # CSV 파일 경로
 data = pd.read_csv(csv_path)
 
 # Hugging Face Dataset으로 변환
 dataset = Dataset.from_pandas(data)
 
-print(dataset[0])
-
-
 # Base Model Setting
 from unsloth import FastLanguageModel
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
 
-model_name = "beomi/Llama-3-KoEn-8B-Instruct-preview"
-max_seq_length = 2048
+model_name = "meta-llama/Llama-3.1-8B"  # Base 모델 경로
+max_seq_length = 512
 dtype = None
 load_in_4bit = True
 
+# Base 모델 및 Tokenizer 로드
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = model_name,
-    max_seq_length = max_seq_length,
-    dtype = dtype,
-    load_in_4bit = load_in_4bit,
+    model_name=model_name,
+    max_seq_length=max_seq_length,
+    dtype=dtype,
+    load_in_4bit=load_in_4bit,
 )
 
 
-# lora adapter 사용 (fine tuning 가속)
+# LoRA 어댑터 설정 (fine-tuning용)
 model = FastLanguageModel.get_peft_model(
     model,
-    r = 16,
-    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
-                      "gate_proj", "up_proj", "down_proj",],
-    lora_alpha = 16,
-    lora_dropout = 0,
-    bias = "none",
-    use_gradient_checkpointing = "unsloth",
-    random_state = 3407,
-    use_rslora = False,
-    loftq_config = None,
+    r=16,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
+                    "gate_proj", "up_proj", "down_proj"],
+    lora_alpha=16,
+    lora_dropout=0,
+    bias="none",
+    use_gradient_checkpointing="unsloth",
+    random_state=3407,
+    use_rslora=False,
+    loftq_config=None,
 )
 
 
-llama_prompt = """아래는 작업을 설명하는 지침과 추가적인 맥락을 제공하는 입력이 주어집니다. 주어진 원문에 없는 내용은 답변으로 절대 제공하지 않습니다.
-
-### instruction:
-{}
-
-### input:
-"주어진 원문은 광주대학교에 대한 정보입니다."
-
-### 응답:
-{}
+# Prompt Template 설정
+llama_prompt = """You must be a good AI assistant that provides information about Gwangju University and answer questions in a question-and-answer format. You must answer as kindly as possible in Korean, and if you don't understand something, you must say, 'I didn't understand well'.
+Question: {instruction} 
+Context: {input} 
+Answer: {output}<|eot_id|><|end_of_text|>
 """
+#llama_prompt = hub.pull("rlm/rag-prompt")
+EOS_TOKEN = tokenizer.eos_token  # EOS 토큰
 
-
-
-
-EOS_TOKEN = tokenizer.eos_token 
+# Prompt 생성 함수
 def formatting_prompts_func(examples):
     instructions = examples["instruction"]
-    inputs = examples["input"]
+    inputs = inputs = [""] * len(examples["instruction"])#examples["input"]
     outputs = examples["output"]
     texts = []
-    for instruction, input, output in zip(instructions, inputs, outputs):
-        text = llama_prompt.format(instruction, input, output) + EOS_TOKEN
+    for instruction, input_text, output_text in zip(instructions, inputs, outputs):
+        text = llama_prompt.format(
+            instruction=instruction,
+            input=input_text,
+            output=output_text
+        ) + EOS_TOKEN
         texts.append(text)
-    return {"text" : texts, }
-pass
+    return {"text": texts}
 
-dataset = dataset.map(formatting_prompts_func, batched = True)
+
+# Dataset 변환
+dataset = dataset.map(formatting_prompts_func, batched=True)
 
 
 # 모델 훈련 세팅
@@ -81,33 +83,34 @@ from transformers import TrainingArguments
 from unsloth import is_bfloat16_supported
 
 trainer = SFTTrainer(
-    model = model,
-    tokenizer = tokenizer,
-    train_dataset = dataset,
-    dataset_text_field = "text",
-    max_seq_length = max_seq_length,
-    dataset_num_proc = 2,
-    packing = False,
-    args = TrainingArguments(
-        per_device_train_batch_size = 2,
-        gradient_accumulation_steps = 4,
-        warmup_steps = 5,
-        max_steps = 60,
-        learning_rate = 2e-4,
-        fp16 = not is_bfloat16_supported(),
-        bf16 = is_bfloat16_supported(),
-        logging_steps = 1,
-        optim = "adamw_8bit",
-        weight_decay = 0.01,
-        lr_scheduler_type = "linear",
-        seed = 3407,
-        output_dir = "outputs",
+    model=model,
+    tokenizer=tokenizer,
+    train_dataset=dataset,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    dataset_num_proc=2,
+    packing=False,
+    args=TrainingArguments(
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
+        warmup_steps=5,
+        max_steps=60,
+        learning_rate=2e-4,
+        fp16=not is_bfloat16_supported(),
+        bf16=is_bfloat16_supported(),
+        logging_steps=1,
+        optim="adamw_8bit",
+        weight_decay=0.01,
+        lr_scheduler_type="linear",
+        seed=3407,
+        output_dir="outputs",
     ),
 )
 
 # 모델 훈련
 trainer_stats = trainer.train()
 print(trainer_stats)
+
 # 모델 저장
 model.save_pretrained("./finetuned_model")
 tokenizer.save_pretrained("./finetuned_model")
--- model_test.py
+++ model_test.py
@@ -7,7 +7,12 @@ from langchain import hub
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableParallel
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
+from langchain.llms import HuggingFacePipeline
+from langchain_core.prompts import ChatPromptTemplate
+
+MODEL_PATH = "./finetuned_model"
+CACHE_DIR = "./cache_model"
 
 VECTOR_STORE_PATH = "./vectorstore"
 EMBEDDINGS = HuggingFaceEmbeddings(
@@ -50,14 +55,36 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
+# 허깅페이스 파이프라인 생성
+def create_huggingface_llm():
+    if os.path.exists(CACHE_DIR):  # 이미 캐시된 모델이 있으면 로드
+        print("Loading model from cache...")
+        model = AutoModelForCausalLM.from_pretrained(CACHE_DIR)
+        tokenizer = AutoTokenizer.from_pretrained(CACHE_DIR)
+    else:
+        print("Loading model from scratch...")
+        model = AutoModelForCausalLM.from_pretrained(MODEL_PATH)
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+        model.save_pretrained(CACHE_DIR)  # 모델을 캐시 디렉토리에 저장
+        tokenizer.save_pretrained(CACHE_DIR)  # 토크나이저를 캐시 디렉토리에 저장
 
-def huggingface_llm(input_data):
-    model_name = "./finetuned_model"  # Hugging Face에 저장한 모델 경로
-    model = AutoModelForCausalLM.from_pretrained(model_name).to("cuda")
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    inputs = tokenizer(input_data, return_tensors="pt")
-    outputs = model.generate(inputs['input_ids'])
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        device=0,  # GPU를 사용할 경우 0, CPU는 -1
+        stop_sequence=["<|eot_id|>", "<|end_of_text|>"],  # 종료 신호
+        max_new_tokens=512,
+        do_sample=True,
+        temperature=0.7,
+        top_p=0.95,
+        top_k=40,
+        repetition_penalty=1.2
+    )
+    
+    # LangChain LLM으로 래핑
+    llm = HuggingFacePipeline(pipeline=pipe)
+    return llm
 
 def main():
     if not os.path.exists(VECTOR_STORE_PATH):
@@ -66,39 +93,45 @@ def main():
         vectorstore = FAISS.load_local(
             VECTOR_STORE_PATH, EMBEDDINGS, allow_dangerous_deserialization=True
         )
-
+    # 문서 로드 후 상위 폴더로 이동
     os.chdir("..")
-
 
     
     # 프롬프트 로드
-    prompt = hub.pull("rlm/rag-prompt")
-
+    #prompt = hub.pull("rlm/rag-prompt")
+    template = """Answer the question based only on the following context:
+    {context}
+    Question: {question}
+    """
+    
+    prompt = ChatPromptTemplate.from_template(template)
     # Ollama 모델 로드
-    llm = ChatOllama(model="llama3.1:8b")
-
-    retriever = vectorstore.as_retriever()
+    #llm = ChatOllama(model="llama3.1:8b")
+    llm = create_huggingface_llm()
+    
+    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 1})
     
     # RAG 체인 구성
     rag_chain = (
-        RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
+        #RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
+        {"context": retriever, "question": RunnablePassthrough()}
         | prompt
         | llm
         | StrOutputParser()
     )
-    rag_chain_with_source = RunnableParallel(
-        {"context": retriever, "question": RunnablePassthrough()}
-    ).assign(answer=rag_chain)
+    #rag_chain_with_source = RunnableParallel(
+    #    {"context": retriever, "question": RunnablePassthrough()}
+    #).assign(answer=rag_chain)
 
     # 모델 테스트
-    query = "휴학은 어떻게 해?"
-    response = rag_chain_with_source.invoke(query)
+    query = "휴학 신청절차 알려줘"
+    response = rag_chain.invoke(query)
 
-    print("Answer:\n", response["answer"] + "\n")
+    print("----------------Answer----------------\n", response + "\n")
     print("Sources:")
-    sources = [doc.metadata for doc in response["context"]]
-    for source in sources:
-        print(source)
+    #sources = [doc.metadata for doc in response["context"]]
+    #for source in sources:
+     #   print(source)
 
 if __name__ == "__main__":
     main()
