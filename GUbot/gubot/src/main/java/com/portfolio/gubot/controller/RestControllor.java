b432967f298d3ce57d89214227363dff6a781104
00Hwang <rahyun3746@gmail.com>
2025-02-20 23:43:02 +0900

    allowPublicKeyRetrieval 설정 추가
    DB.url에 allowPublicKeyRetrieval=true를 추가함.

--- GUbot/gubot/src/main/java/com/portfolio/gubot/controller/RestControllor.java
+++ GUbot/gubot/src/main/java/com/portfolio/gubot/controller/RestControllor.java
@@ -1,13 +1,12 @@
 package com.portfolio.gubot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class RestControllor {
 
-    @RequestMapping(value = "/valueTest", method=RequestMethod.GET)
+    @GetMapping("/valueTest")
     public String restTest() {
         String value = "RestControllor";
         return value;
